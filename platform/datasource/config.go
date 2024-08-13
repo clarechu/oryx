c12b0258805c83aae9c456750eccf9512cfcd8d3
chulei <1062186165@qq.com>
2024-08-13 20:44:47 +0800

    修改文件依赖项 抽象成当前redis datasource

--- platform/datasource/config.go
+++ platform/datasource/config.go
@@ -6,7 +6,7 @@ type Config struct {
 	ID        uint      `json:"id,omitempty" gorm:"primary_key;autoIncrement"`
 	Key       string    `json:"key,omitempty" gorm:"column:c_key;index:idx_keys"`
 	Field     string    `json:"field,omitempty" gorm:"column:c_field;index:idx_keys"`
-	Value     string    `json:"value,omitempty" gorm:"column:c_value;size:1024"`
+	Value     string    `json:"value,omitempty" gorm:"column:c_value;type:text"`
 	CreatedAt time.Time `json:"created_at,omitempty" gorm:"column:created_at;autoCreateTime:nano"`
 	UpdatedAt time.Time `json:"updated_at,omitempty" gorm:"column:updated_at;autoUpdateTime:nano"`
 }
